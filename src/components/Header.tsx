6c0bc772e1509d0ef010b921ccea52e16f4780c6
Ali-Asif1 <aliasif1485@yahoo.com>
2024-12-04 18:44:09 +0500

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import Logo from '../../public/Logo.png'
+import Logo from '../../public/logo.png'
 
 export const Header = () => {
   return (
